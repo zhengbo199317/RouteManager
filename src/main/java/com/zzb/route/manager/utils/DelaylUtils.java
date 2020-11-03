ee2419e51a085495ff9a3fa02db5ea43eb1de822
473305694@qq.com <z473305694>
2020-11-03 17:03:34 +0800

    gengxin

--- src/main/java/com/zzb/route/manager/utils/DelaylUtils.java
+++ src/main/java/com/zzb/route/manager/utils/DelaylUtils.java
@@ -3,15 +3,14 @@ package com.zzb.route.manager.utils;
 import com.google.gson.Gson;
 import com.zzb.route.manager.dataObject.RouteDetail;
 import com.zzb.route.manager.service.RouteDetailService;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /*
 服务器连接测试
@@ -34,22 +33,43 @@ public class DelaylUtils {
         }else{
             routeDetail = gson.fromJson(routeStr, RouteDetail.class);
         }
-        CloseableHttpClient httpClient = HttpClients.createDefault();
         long dif = 0;
         try {
-//            System.out.println(Thread.currentThread().getName()+"开始了");
-            HttpGet httpGet = new HttpGet(routeDetail.getRouteUrl());
+            URL url=new URL(routeDetail.getRouteUrl());
             long start = System.currentTimeMillis();
-            CloseableHttpResponse execute = httpClient.execute(httpGet);
-            long end = System.currentTimeMillis();
-            dif = end - start;
-            routeDetail.setRouteDelay(String.valueOf(dif)+"ms").setRouteStatus("服务正常");
-            ops.set(routeDetail.getRouteName(), gson.toJson(routeDetail));
+            HttpURLConnection con=(HttpURLConnection)url.openConnection();
+            con.setConnectTimeout(3000);
+            if (con.getResponseCode()>200){
+                routeDetail.setRouteDelay("-1").setRouteStatus("服务异常");
+                ops.set(routeDetail.getRouteName(), gson.toJson(routeDetail));
+            }else{
+                long end = System.currentTimeMillis();
+                dif = end - start;
+                routeDetail.setRouteDelay(String.valueOf(dif)+"ms").setRouteStatus("服务正常");
+                ops.set(routeDetail.getRouteName(), gson.toJson(routeDetail));
+            }
         } catch (Exception e) {
             //连接失败
             routeDetail.setRouteDelay("-1").setRouteStatus("服务异常");
             ops.set(routeDetail.getRouteName(), gson.toJson(routeDetail));
         }
-//        System.out.println(Thread.currentThread().getName()+"结束了");
+    }
+
+    public static void main(String[] args) {
+        try {
+            long start = System.currentTimeMillis();
+            URL url=new URL("http://192.168.0.183:8080/jsaas");
+            HttpURLConnection connection=(HttpURLConnection)url.openConnection();
+            connection.setConnectTimeout(3000);
+            if (connection.getResponseCode()>200){
+                System.out.println(false);
+            }else{
+                System.out.println(true);
+            }
+            long end = System.currentTimeMillis();
+            System.out.println(end-start);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
